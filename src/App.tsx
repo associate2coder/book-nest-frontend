72623a7fe620216eb32f85f1bafae7227a375183
associate2coder <olatsko@gmail.com>
2025-02-17 14:04:44 +0200

    test commumnication with backend

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,42 @@
+import { useEffect, useState } from 'react';
 import constructionImage from '../src/assets/under-construction.svg';
 import './App.scss';
 
+const TEST_URL = '/api/health';
+
+
 export const App: React.FC = () => {
+  const [testMessage, setTestMessage] = useState('');
+
+  useEffect(() => {
+    fetch(TEST_URL)
+      .then(res => {
+        console.log(res);
+        
+        if (!res.ok) {
+          throw new Error('Request' + res.status);
+        }
+
+        return res.text();
+      })
+      .then(data => {
+        console.log(data);
+
+        setTestMessage(data);
+      })
+      .catch(err => {
+        setTestMessage(`Error: ${err}`);
+      })
+  }, []);
+
   return (
     <div className="App">
 
       <div className="under-construction-container">
-        <h1>BookNest</h1>
+        <h1>{testMessage}</h1>
+
         <img src={constructionImage} alt="page under construction image" />
+
         <p className="under-construction-message">This page is under construction</p>
       </div>
     </div>
--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://ec2-3-84-50-78.compute-1.amazonaws.com:8080',
+        changeOrigin: true,
+        rewrite: path => path.replace(/^\/api/, '')
+      }
+    }
+  }
 })
