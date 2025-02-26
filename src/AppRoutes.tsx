d5b2f4c5d43bf20b568dce0b1fe8150f397a3331
associate2coder <olatsko@gmail.com>
2025-02-26 20:36:18 +0200

    data deletion instruction

--- src/AppRoutes.tsx
+++ src/AppRoutes.tsx
@@ -1,11 +1,13 @@
 import { Route, Routes } from "react-router-dom";
 import { App } from "./App";
 import { PrivacyPolicy } from "./modules/PrivacyPolicy/PrivacyPolicy";
+import { DataDeletion } from "./modules/DataDeletion";
 
 export const AppRoutes = () => {
   return (
     <Routes>
       <Route path="/privacy-policy" element={<PrivacyPolicy />} />
+      <Route path="/data-deletion" element={<DataDeletion />} />
       <Route path="/" element={<App />}>
         {/* <Route index element={<HomePage />} /> */}
 
