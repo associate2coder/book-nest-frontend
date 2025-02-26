ed608035e48fbf4d39294e7ddc6ccd0cdf8f1577
associate2coder <olatsko@gmail.com>
2025-02-26 17:34:55 +0200

    add privacy info

--- src/AppRoutes.tsx
+++ src/AppRoutes.tsx
@@ -1,15 +1,18 @@
 import { Route, Routes } from "react-router-dom";
 import { App } from "./App";
+import { PrivacyPolicy } from "./modules/PrivacyPolicy/PrivacyPolicy";
 
 export const AppRoutes = () => {
   return (
     <Routes>
+      <Route path="/privacy-policy" element={<PrivacyPolicy />} />
       <Route path="/" element={<App />}>
         {/* <Route index element={<HomePage />} /> */}
 
         {/* More Routes to be added here */}
 
         {/* <Route path="*" element={<PageNotFound />} /> */}
+
       </Route>
     </Routes>
   );
---
+++ src/modules/PrivacyPolicy/PrivacyPolicy.tsx
@@ -0,0 +1,25 @@
+import { useEffect, useState } from "react";
+import styles from './PrivacyPolicy.module.scss';
+
+export const PrivacyPolicy: React.FC = () => {
+
+  const [policy, setPolicy] = useState<string[]>([]);
+
+  useEffect(() => {
+    fetch('privacy.json')
+      .then(res => {
+        if (res.ok) {
+          return res.json();
+        }
+      })
+      .then(data => setPolicy(data.text || []))
+  }, [])
+
+  return (
+    <div className={styles.container}>
+      {policy.map((paragraph, i) => (
+        <p key={`${i}.${i}`} className={styles.paragraph}>{paragraph}</p>
+      ))}
+    </div>
+  );
+}
