50d96ae601a633182bfd3ebc7d41ac6d2ef70feb
associate2coder <olatsko@gmail.com>
2025-03-24 11:55:15 +0200

    modify privacy policy content

--- src/modules/DataDeletion/DataDeletion.tsx
+++ src/modules/DataDeletion/DataDeletion.tsx
@@ -1,11 +1,16 @@
 import { useEffect, useState } from "react";
 import styles from './DataDeletion.module.scss';
+import { Link } from "react-router-dom";
+import { Loader } from "../../shared/components/Loader";
 
 export const DataDeletion: React.FC = () => {
 
   const [instruction, setInstruction] = useState<string[]>([]);
+  const [loaded, setLoaded] = useState(false);
 
   useEffect(() => {
+    setLoaded(false);
+    
     fetch('instruction.json')
       .then(res => {
         if (res.ok) {
@@ -13,11 +18,23 @@ export const DataDeletion: React.FC = () => {
         }
       })
       .then(data => setInstruction(data.text || []))
+      .finally(() => setLoaded(true));
   }, [])
 
   return (
     <div className={styles.container}>
-      {instruction.map((paragraph, i) => (
+      <h1 className={styles.title}>Data Deletion Instructions</h1>
+
+      <Link 
+        to="/privacy-policy"
+        className={styles.link}
+        >
+          Back to Privacy Policy and Cookies
+      </Link>
+
+      {!loaded && <Loader />}
+
+      {loaded && instruction.map((paragraph, i) => (
         <p key={`${i}.${i}`} className={styles.paragraph}>{paragraph}</p>
       ))}
     </div>
--- src/modules/PrivacyPolicy/PrivacyPolicy.tsx
+++ src/modules/PrivacyPolicy/PrivacyPolicy.tsx
@@ -1,11 +1,16 @@
 import { useEffect, useState } from "react";
 import styles from './PrivacyPolicy.module.scss';
+import { Link } from "react-router-dom";
+import { Loader } from "../../shared/components/Loader";
 
 export const PrivacyPolicy: React.FC = () => {
 
   const [policy, setPolicy] = useState<string[]>([]);
+  const [loaded, setLoaded] = useState(false);
 
   useEffect(() => {
+    setLoaded(false);
+
     fetch('privacy.json')
       .then(res => {
         if (res.ok) {
@@ -13,11 +18,24 @@ export const PrivacyPolicy: React.FC = () => {
         }
       })
       .then(data => setPolicy(data.text || []))
+      .finally(() => setLoaded(true));
   }, [])
 
   return (
     <div className={styles.container}>
-      {policy.map((paragraph, i) => (
+
+      <h1 className={styles.title}>Privacy Policy and Cookies</h1>
+
+      <Link 
+        to="/data-deletion"
+        className={styles.link}
+        >
+          Please click here for more information for data deletion instruction
+      </Link>
+
+      {!loaded && <Loader />}
+
+      {loaded && policy.map((paragraph, i) => (
         <p key={`${i}.${i}`} className={styles.paragraph}>{paragraph}</p>
       ))}
     </div>
